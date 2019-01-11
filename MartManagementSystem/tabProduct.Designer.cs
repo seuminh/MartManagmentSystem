e2e0f11ffda26b75c8d05e01316aaccd369e455f
Seuminh <seuminh@yahoo.com>
2019-01-11 21:07:14 +0700

    Redesign tabProduct Header

--- MartManagementSystem/tabProduct.Designer.cs
+++ MartManagementSystem/tabProduct.Designer.cs
@@ -33,10 +33,11 @@
             this.panelShow = new Bunifu.Framework.UI.BunifuGradientPanel();
             this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.btnAddProduct = new Bunifu.Framework.UI.BunifuTileButton();
-            this.btnEditProduct = new Bunifu.Framework.UI.BunifuTileButton();
-            this.btnDeleteProduct = new Bunifu.Framework.UI.BunifuTileButton();
-            this.btnSearchProduct = new Bunifu.Framework.UI.BunifuTileButton();
+            this.btnSearchProduct = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnDeleteProduct = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnEditProduct = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnAddProduct = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelShow
@@ -63,106 +64,171 @@
             // 
             // panelMenu
             // 
-            this.panelMenu.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.panelMenu.Controls.Add(this.btnSearchProduct);
+            this.panelMenu.Controls.Add(this.btnAddProduct);
+            this.panelMenu.Controls.Add(this.btnDeleteProduct);
+            this.panelMenu.Controls.Add(this.btnEditProduct);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(646, 118);
             this.panelMenu.TabIndex = 7;
             // 
-            // btnAddProduct
+            // btnSearchProduct
             // 
-            this.btnAddProduct.BackColor = System.Drawing.Color.SeaGreen;
-            this.btnAddProduct.color = System.Drawing.Color.SeaGreen;
-            this.btnAddProduct.colorActive = System.Drawing.Color.MediumSeaGreen;
-            this.btnAddProduct.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAddProduct.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddProduct.ForeColor = System.Drawing.Color.White;
-            this.btnAddProduct.Image = ((System.Drawing.Image)(resources.GetObject("btnAddProduct.Image")));
-            this.btnAddProduct.ImagePosition = 0;
-            this.btnAddProduct.ImageZoom = 0;
-            this.btnAddProduct.LabelPosition = 34;
-            this.btnAddProduct.LabelText = "Add";
-            this.btnAddProduct.Location = new System.Drawing.Point(101, 39);
-            this.btnAddProduct.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnAddProduct.Name = "btnAddProduct";
-            this.btnAddProduct.Size = new System.Drawing.Size(88, 45);
-            this.btnAddProduct.TabIndex = 2;
-            this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
-            // 
-            // btnEditProduct
-            // 
-            this.btnEditProduct.BackColor = System.Drawing.Color.SeaGreen;
-            this.btnEditProduct.color = System.Drawing.Color.SeaGreen;
-            this.btnEditProduct.colorActive = System.Drawing.Color.MediumSeaGreen;
-            this.btnEditProduct.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnEditProduct.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditProduct.ForeColor = System.Drawing.Color.White;
-            this.btnEditProduct.Image = ((System.Drawing.Image)(resources.GetObject("btnEditProduct.Image")));
-            this.btnEditProduct.ImagePosition = 0;
-            this.btnEditProduct.ImageZoom = 0;
-            this.btnEditProduct.LabelPosition = 34;
-            this.btnEditProduct.LabelText = "Edit";
-            this.btnEditProduct.Location = new System.Drawing.Point(211, 39);
-            this.btnEditProduct.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnEditProduct.Name = "btnEditProduct";
-            this.btnEditProduct.Size = new System.Drawing.Size(88, 45);
-            this.btnEditProduct.TabIndex = 3;
-            this.btnEditProduct.Click += new System.EventHandler(this.btnEditProduct_Click);
+            this.btnSearchProduct.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(170)))), ((int)(((byte)(242)))));
+            this.btnSearchProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(103)))), ((int)(((byte)(214)))));
+            this.btnSearchProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSearchProduct.BorderRadius = 0;
+            this.btnSearchProduct.ButtonText = "     Search";
+            this.btnSearchProduct.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearchProduct.DisabledColor = System.Drawing.Color.Gray;
+            this.btnSearchProduct.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSearchProduct.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnSearchProduct.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnSearchProduct.Iconimage")));
+            this.btnSearchProduct.Iconimage_right = null;
+            this.btnSearchProduct.Iconimage_right_Selected = null;
+            this.btnSearchProduct.Iconimage_Selected = null;
+            this.btnSearchProduct.IconMarginLeft = 0;
+            this.btnSearchProduct.IconMarginRight = 0;
+            this.btnSearchProduct.IconRightVisible = true;
+            this.btnSearchProduct.IconRightZoom = 0D;
+            this.btnSearchProduct.IconVisible = true;
+            this.btnSearchProduct.IconZoom = 50D;
+            this.btnSearchProduct.IsTab = false;
+            this.btnSearchProduct.Location = new System.Drawing.Point(463, 39);
+            this.btnSearchProduct.Name = "btnSearchProduct";
+            this.btnSearchProduct.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(103)))), ((int)(((byte)(214)))));
+            this.btnSearchProduct.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(170)))), ((int)(((byte)(242)))));
+            this.btnSearchProduct.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnSearchProduct.selected = false;
+            this.btnSearchProduct.Size = new System.Drawing.Size(116, 48);
+            this.btnSearchProduct.TabIndex = 7;
+            this.btnSearchProduct.Text = "     Search";
+            this.btnSearchProduct.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnSearchProduct.Textcolor = System.Drawing.Color.White;
+            this.btnSearchProduct.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchProduct.Click += new System.EventHandler(this.btnSearchProduct_Click);
             // 
             // btnDeleteProduct
             // 
-            this.btnDeleteProduct.BackColor = System.Drawing.Color.SeaGreen;
-            this.btnDeleteProduct.color = System.Drawing.Color.SeaGreen;
-            this.btnDeleteProduct.colorActive = System.Drawing.Color.MediumSeaGreen;
+            this.btnDeleteProduct.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(71)))), ((int)(((byte)(87)))));
+            this.btnDeleteProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(91)))), ((int)(((byte)(101)))));
+            this.btnDeleteProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDeleteProduct.BorderRadius = 0;
+            this.btnDeleteProduct.ButtonText = "      Delete";
             this.btnDeleteProduct.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDeleteProduct.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDeleteProduct.ForeColor = System.Drawing.Color.White;
-            this.btnDeleteProduct.Image = ((System.Drawing.Image)(resources.GetObject("btnDeleteProduct.Image")));
-            this.btnDeleteProduct.ImagePosition = 0;
-            this.btnDeleteProduct.ImageZoom = 0;
-            this.btnDeleteProduct.LabelPosition = 34;
-            this.btnDeleteProduct.LabelText = "Delete";
-            this.btnDeleteProduct.Location = new System.Drawing.Point(321, 39);
-            this.btnDeleteProduct.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDeleteProduct.DisabledColor = System.Drawing.Color.Gray;
+            this.btnDeleteProduct.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnDeleteProduct.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnDeleteProduct.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnDeleteProduct.Iconimage")));
+            this.btnDeleteProduct.Iconimage_right = null;
+            this.btnDeleteProduct.Iconimage_right_Selected = null;
+            this.btnDeleteProduct.Iconimage_Selected = null;
+            this.btnDeleteProduct.IconMarginLeft = 0;
+            this.btnDeleteProduct.IconMarginRight = 0;
+            this.btnDeleteProduct.IconRightVisible = true;
+            this.btnDeleteProduct.IconRightZoom = 0D;
+            this.btnDeleteProduct.IconVisible = true;
+            this.btnDeleteProduct.IconZoom = 50D;
+            this.btnDeleteProduct.IsTab = false;
+            this.btnDeleteProduct.Location = new System.Drawing.Point(328, 39);
             this.btnDeleteProduct.Name = "btnDeleteProduct";
-            this.btnDeleteProduct.Size = new System.Drawing.Size(88, 45);
-            this.btnDeleteProduct.TabIndex = 4;
+            this.btnDeleteProduct.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(91)))), ((int)(((byte)(101)))));
+            this.btnDeleteProduct.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(71)))), ((int)(((byte)(87)))));
+            this.btnDeleteProduct.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnDeleteProduct.selected = false;
+            this.btnDeleteProduct.Size = new System.Drawing.Size(116, 48);
+            this.btnDeleteProduct.TabIndex = 6;
+            this.btnDeleteProduct.Text = "      Delete";
+            this.btnDeleteProduct.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnDeleteProduct.Textcolor = System.Drawing.Color.White;
+            this.btnDeleteProduct.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDeleteProduct.Click += new System.EventHandler(this.btnDeleteProduct_Click);
             // 
-            // btnSearchProduct
+            // btnEditProduct
             // 
-            this.btnSearchProduct.BackColor = System.Drawing.Color.SeaGreen;
-            this.btnSearchProduct.color = System.Drawing.Color.SeaGreen;
-            this.btnSearchProduct.colorActive = System.Drawing.Color.MediumSeaGreen;
-            this.btnSearchProduct.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSearchProduct.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearchProduct.ForeColor = System.Drawing.Color.White;
-            this.btnSearchProduct.Image = ((System.Drawing.Image)(resources.GetObject("btnSearchProduct.Image")));
-            this.btnSearchProduct.ImagePosition = 0;
-            this.btnSearchProduct.ImageZoom = 0;
-            this.btnSearchProduct.LabelPosition = 34;
-            this.btnSearchProduct.LabelText = "Search";
-            this.btnSearchProduct.Location = new System.Drawing.Point(431, 39);
-            this.btnSearchProduct.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnSearchProduct.Name = "btnSearchProduct";
-            this.btnSearchProduct.Size = new System.Drawing.Size(88, 45);
-            this.btnSearchProduct.TabIndex = 5;
-            this.btnSearchProduct.Click += new System.EventHandler(this.btnSearchProduct_Click);
+            this.btnEditProduct.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(166)))), ((int)(((byte)(157)))));
+            this.btnEditProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(98)))), ((int)(((byte)(117)))));
+            this.btnEditProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEditProduct.BorderRadius = 0;
+            this.btnEditProduct.ButtonText = "      Edit";
+            this.btnEditProduct.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditProduct.DisabledColor = System.Drawing.Color.Gray;
+            this.btnEditProduct.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEditProduct.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnEditProduct.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnEditProduct.Iconimage")));
+            this.btnEditProduct.Iconimage_right = null;
+            this.btnEditProduct.Iconimage_right_Selected = null;
+            this.btnEditProduct.Iconimage_Selected = null;
+            this.btnEditProduct.IconMarginLeft = 0;
+            this.btnEditProduct.IconMarginRight = 0;
+            this.btnEditProduct.IconRightVisible = true;
+            this.btnEditProduct.IconRightZoom = 0D;
+            this.btnEditProduct.IconVisible = true;
+            this.btnEditProduct.IconZoom = 50D;
+            this.btnEditProduct.IsTab = false;
+            this.btnEditProduct.Location = new System.Drawing.Point(193, 39);
+            this.btnEditProduct.Name = "btnEditProduct";
+            this.btnEditProduct.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(98)))), ((int)(((byte)(117)))));
+            this.btnEditProduct.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(166)))), ((int)(((byte)(157)))));
+            this.btnEditProduct.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnEditProduct.selected = false;
+            this.btnEditProduct.Size = new System.Drawing.Size(116, 48);
+            this.btnEditProduct.TabIndex = 5;
+            this.btnEditProduct.Text = "      Edit";
+            this.btnEditProduct.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnEditProduct.Textcolor = System.Drawing.Color.White;
+            this.btnEditProduct.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditProduct.Click += new System.EventHandler(this.btnEditProduct_Click);
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(227)))), ((int)(((byte)(116)))));
+            this.btnAddProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(148)))), ((int)(((byte)(50)))));
+            this.btnAddProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAddProduct.BorderRadius = 0;
+            this.btnAddProduct.ButtonText = "      Add";
+            this.btnAddProduct.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddProduct.DisabledColor = System.Drawing.Color.Gray;
+            this.btnAddProduct.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAddProduct.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnAddProduct.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnAddProduct.Iconimage")));
+            this.btnAddProduct.Iconimage_right = null;
+            this.btnAddProduct.Iconimage_right_Selected = null;
+            this.btnAddProduct.Iconimage_Selected = null;
+            this.btnAddProduct.IconMarginLeft = 0;
+            this.btnAddProduct.IconMarginRight = 0;
+            this.btnAddProduct.IconRightVisible = true;
+            this.btnAddProduct.IconRightZoom = 0D;
+            this.btnAddProduct.IconVisible = true;
+            this.btnAddProduct.IconZoom = 50D;
+            this.btnAddProduct.IsTab = false;
+            this.btnAddProduct.Location = new System.Drawing.Point(58, 39);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(148)))), ((int)(((byte)(50)))));
+            this.btnAddProduct.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(227)))), ((int)(((byte)(116)))));
+            this.btnAddProduct.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnAddProduct.selected = false;
+            this.btnAddProduct.Size = new System.Drawing.Size(116, 48);
+            this.btnAddProduct.TabIndex = 4;
+            this.btnAddProduct.Text = "      Add";
+            this.btnAddProduct.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnAddProduct.Textcolor = System.Drawing.Color.White;
+            this.btnAddProduct.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
             // 
             // tabProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panelShow);
-            this.Controls.Add(this.btnSearchProduct);
-            this.Controls.Add(this.btnDeleteProduct);
-            this.Controls.Add(this.btnEditProduct);
-            this.Controls.Add(this.btnAddProduct);
             this.Controls.Add(this.panelMenu);
             this.Name = "tabProduct";
             this.Size = new System.Drawing.Size(646, 656);
             this.Load += new System.EventHandler(this.tabProduct_Load);
+            this.panelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -170,10 +236,10 @@
         #endregion
         private Bunifu.Framework.UI.BunifuGradientPanel panelShow;
         private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
-        private Bunifu.Framework.UI.BunifuTileButton btnSearchProduct;
-        private Bunifu.Framework.UI.BunifuTileButton btnDeleteProduct;
-        private Bunifu.Framework.UI.BunifuTileButton btnEditProduct;
-        private Bunifu.Framework.UI.BunifuTileButton btnAddProduct;
         private System.Windows.Forms.Panel panelMenu;
+        private Bunifu.Framework.UI.BunifuFlatButton btnSearchProduct;
+        private Bunifu.Framework.UI.BunifuFlatButton btnAddProduct;
+        private Bunifu.Framework.UI.BunifuFlatButton btnDeleteProduct;
+        private Bunifu.Framework.UI.BunifuFlatButton btnEditProduct;
     }
 }
